8d2f4ad5b339be894fb7e2f8da34350f92f17229
lusorich <dreamonautre@gmail.com>
2020-07-23 19:10:55 +0500

    css modules

--- simple-react-hw/src/p2-homeworks/h1/Message.tsx
+++ simple-react-hw/src/p2-homeworks/h1/Message.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./message.css";
+import styles from "./Message.module.css";
 
 type PropsType = {
     avatar: string
@@ -11,11 +11,11 @@ type PropsType = {
 
 function Message(props: PropsType) {
     return (
-        <div className="message">
-            <img className="message__avatar" src={props.avatar} width="50" height="50" alt="Аватар"></img>
-            <h2 className="message__title">{props.name}</h2>
-            <p className="message__text">{props.message}</p>
-            <span className="message__time">{props.time}</span>
+        <div className={styles.message}>
+            <img className={styles.message__avatar} src={props.avatar} width="50" height="50" alt="Аватар"></img>
+            <h2 className={styles.message__title}>{props.name}</h2>
+            <p className={styles.message__text}>{props.message}</p>
+            <span className={styles.message__time}>{props.time}</span>
         </div>
     );
 }
