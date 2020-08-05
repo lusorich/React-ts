1f5df2ff4562225dba08692f7940abea9a714e69
lusorich <dreamonautre@gmail.com>
2020-08-05 22:21:16 +0500

    n

--- videos-course-1/src/components/Dialogs/dialogs.tsx
+++ videos-course-1/src/components/Dialogs/dialogs.tsx
@@ -47,18 +47,15 @@ const Dialogs = () => {
         { id: 2, message: 'Hi' },
         { id: 3, message: 'Konichiwa' },
         { id: 4, message: 'Guten Tag' },
-    ]
+    ];
+
+    const dialogsArray: any = dialogsData.map((el) => <div className={s.dialogItem}> <DialogItem name={el.name} id={el.id}/></div>);
+    const messageArray: any = messagesData.map((el) => <Message message={el.message} id={el.id} />);
 
     return (
         <div className={s.dialogList}>
-            {dialogsData.map((el) => {
-                return (<div className={s.dialogItem}>
-                    <DialogItem name={el.name} id={el.id} />
-                    <Message message={messagesData[el.id - 1].message} id={el.id} />
-                </div>
-                )
-            })
-            }
+                {dialogsArray}
+                {messageArray}
         </div>
     )
 }
