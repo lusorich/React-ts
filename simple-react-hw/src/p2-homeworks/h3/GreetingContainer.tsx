ea734373b4147c20f19af6f07dc1f0a0a43c19a9
lusorich <dreamonautre@gmail.com>
2020-08-08 23:54:51 +0500

    change

---
+++ simple-react-hw/src/p2-homeworks/h3/GreetingContainer.tsx
@@ -0,0 +1,48 @@
+import React, {useState, ChangeEvent} from "react";
+import Greeting from "./Greeting";
+import { UserType } from "./HW3";
+
+type GreetingContainerPropsType = {
+    users: Array<UserType>// need to fix any
+    addUserCallback: (name: string) => void // need to fix any
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+// уровень локальной логики
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
+    const [name, setName] = useState<string>(""); // need to fix any
+    const [error, setError] = useState<string>(""); // need to fix any
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        if (e.currentTarget.value.trim()) {
+            setName(e.currentTarget.value.trim());
+        } else {
+            setName("")
+        }
+    };
+    const addUser = () => {
+        if (name) {
+            addUserCallback(name);
+            alert(`Hello ${name}`);
+        } else {
+            setError("error");
+        } // need to fix
+    };
+
+    const totalUsers = users.length; // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            error={error}
+            totalUsers={totalUsers}
+        />
+    );
+}
+
+export default GreetingContainer;
---
+++ simple-react-hw/src/p2-homeworks/h3/HW3.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import GreetingContainer from "./GreetingContainer";
+import {v1} from "uuid";
+
+// types
+export type UserType = {
+    _id: string // need to fix any
+    name: string // need to fix any
+}
+
+// уровень работы с глобальными данными
+function HW3() {
+    const [users, setUsers] = useState<Array<UserType>>([]); // need to fix any
+
+    const addUserCallback = (name: string) => { // need to fix any
+        const newUser = {_id: v1(), name: name};
+        const newUsers = [newUser, ...users];
+        setUsers(newUsers); // need to fix
+    }
+
+    return (
+        <div>
+            <hr/>
+            homeworks 3
+
+            {/*should work (должно работать)*/}
+            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeGreeting/>*/}
+            <hr/>
+        </div>
+    );
+}
+
+export default HW3;
