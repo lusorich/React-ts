a27af60bdd3a77571fb436c9f2663799bb4fc357
lusorich <dreamonautre@gmail.com>
2020-09-26 12:53:47 +0500

    50 lessons

--- videos-course-1/src/components/Users/Users.tsx
+++ videos-course-1/src/components/Users/Users.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import axios from 'axios';
+import userPhoto from '../../assets/images/nagiev.jpg';
 
 const Users = (props: any) => {
+
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            console.log(response.data.items);
+            let users = response.data.items
+            props.setUsers(users);
+        });
+    }
+
+    const styleImg = {
+        width: '100px',
+        height: '100px'
+    }
+
     return (
         <div>
             {props.users.map((user: any) =>
                 <ul key={user.id}>
                     <li>
                         <div>
-                            <img src={user.imageUrl}></img>
+                            <img style={styleImg} src={user.photos.small == null ? userPhoto : user.photos.small}></img>
                             <button onClick={() => { props.onChangeFollowed(user.id) }}>{user.followed ? 'Follow' : 'Unfollow'}</button>
                         </div>
                         <div>
-                            <span>{user.fullName}</span>
+                            <span>{user.name}</span>
                             <p>{user.status}</p>
-                            <span>{user.location.country}</span>
-                            <span>{user.location.city}</span>
                         </div>
                     </li>
                 </ul>
--- videos-course-1/src/redux/users-reducer.ts
+++ videos-course-1/src/redux/users-reducer.ts
@@ -24,18 +24,18 @@ export const setUsersCreator = (users: any): setUsers => ({
 
 const initialState = {
     users: [
-        {
-            id: 1, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
-            followed: true, fullName: 'Mark', status: 'I am a boss', location: { city: 'Ufa', country: 'Russia' }
-        },
-        {
-            id: 2, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
-            followed: true, fullName: 'John', status: 'I am a noob', location: { city: 'Orenburg', country: 'Russia' }
-        },
-        {
-            id: 3, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
-            followed: true, fullName: 'Keira', status: 'I am a girl', location: { city: 'Moscow', country: 'Russia' }
-        }
+        // {
+        //     id: 1, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
+        //     followed: true, fullName: 'Mark', status: 'I am a boss', location: { city: 'Ufa', country: 'Russia' }
+        // },
+        // {
+        //     id: 2, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
+        //     followed: true, fullName: 'John', status: 'I am a noob', location: { city: 'Orenburg', country: 'Russia' }
+        // },
+        // {
+        //     id: 3, photoUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fru.hellomagazine.com%2Fthumb%2F640x465_5%2Fimages%2F2018%2Fjanuary%2Fmarina%2Fnagiev11012018-zahod.jpg&imgrefurl=https%3A%2F%2Fru.hellomagazine.com%2Fkino-i-televidenie%2Fserialy%2F37880-dmitriy-nagiev-o-novom-seriale-so-svoim-uchastiem-rabote-s-synom-kirillom-i-perezhitom-karantine.html&tbnid=RCJqUHMsA7Vf6M&vet=12ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ..i&docid=gvz_h56GVSK33M&w=640&h=465&q=%D0%BD%D0%B0%D0%B3%D0%B8%D0%B5%D0%B2&ved=2ahUKEwihztOLuP_rAhVTgosKHY2eBzUQMygDegUIARDTAQ',
+        //     followed: true, fullName: 'Keira', status: 'I am a girl', location: { city: 'Moscow', country: 'Russia' }
+        // }
     ]
 }
 
