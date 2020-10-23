648d3f1b2a5e4993f5778b2cc230c8d3431f88c9
lusorich <dreamonautre@gmail.com>
2020-10-23 14:24:41 +0500

    changes

--- incubator-todolist-ts-13/src/api/todolists-api.ts
+++ incubator-todolist-ts-13/src/api/todolists-api.ts
@@ -8,27 +8,49 @@ const settings = {
     }
 }
 
-export type TodolistType = {
+export type todolistType = {
     id: string,
     title: string,
     addedDate: string,
     order: number
 }
 
+type responseType<D> = {
+    resultCode: number,
+    messages: Array<string>,
+    data: D
+}
+
 export const todolistsAPI = {
     getTodolists() {
-        return axios.get<Array<TodolistType>>("https://social-network.samuraijs.com/api/1.1/todo-lists", settings);
+        return axios.get<Array<todolistType>>("https://social-network.samuraijs.com/api/1.1/todo-lists", settings);
     },
     createTodolist(newTitle: string) {
-        return axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists", {
+        return axios.post<responseType<{ item: todolistType }>>("https://social-network.samuraijs.com/api/1.1/todo-lists", {
             title: newTitle
         }, settings)
     },
     deleteTodoilst(todolistId: string) {
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
+        return axios.delete<responseType<{}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, settings)
     },
     updateTodolist(todolistId: string, newTitle: string) {
-        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, { title: newTitle }, settings)
+        return axios.put<responseType<{}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}`, { title: newTitle }, settings)
+    },
+    getTasks(todolistId: string) {
+        return axios.get("https://social-network.samuraijs.com/api/1.1/todo-lists/" + todolistId + "/tasks", settings);
+    },
+    createTask(todolistId: string, title: string) {
+        return axios.post("https://social-network.samuraijs.com/api/1.1/todo-lists/" + todolistId + "/tasks", {
+            title: title
+        }, settings);
+    },
+    updateTask(todolistId: string, newTitle: string) {
+        return axios.put("https://social-network.samuraijs.com/api/1.1/todo-lists/" + todolistId + "/tasks", {
+            title: newTitle
+        }, settings);
+    },
+    deleteTask(todolistId: string, taskId: string) {
+        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistId}/tasks/${taskId}`, settings)
     }
 
 }
--- incubator-todolist-ts-13/src/stories/todolists-api.stories.tsx
+++ incubator-todolist-ts-13/src/stories/todolists-api.stories.tsx
@@ -42,3 +42,43 @@ export const UpdateTodolistTitle = () => {
 
     return <div> {JSON.stringify(state)}</div>
 }
+export const GetTasks = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        todolistsAPI.getTasks('8eecd9eb-34cb-45d3-923c-975dcd3704e4').then(res => setState(res.data));
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const CreateTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        todolistsAPI.createTask('8eecd9eb-34cb-45d3-923c-975dcd3704e4', 'newTask').then(res => setState(res.data));
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const UpdateTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        todolistsAPI.createTask('8eecd9eb-34cb-45d3-923c-975dcd3704e4', 'newTask2').then(res => setState(res.data));
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
+export const DeleteTask = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        const todolistId = '';
+        todolistsAPI.deleteTask('8eecd9eb-34cb-45d3-923c-975dcd3704e4', '7d68bdac-51e1-44f8-8c70-02a25a3793ee').then(res => setState(res.data));
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
