055510eeb8c4b972386a637eb7528f59c1ac2c6d
lusorich <dreamonautre@gmail.com>
2021-01-11 01:56:43 +0500

    Updates

---
+++ precache-manifest.41ec3b3c3c559c8d35ab4213d2d2ca8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7199e4b22778ccc2cda8d14a2c8f73ff",
+    "url": "/React-ts/simple-react-hw/index.html"
+  },
+  {
+    "revision": "d51de1dad4188e2eaf9b",
+    "url": "/React-ts/simple-react-hw/static/css/main.46a58908.chunk.css"
+  },
+  {
+    "revision": "530f9d853a3f880bfa3b",
+    "url": "/React-ts/simple-react-hw/static/js/2.902cccfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ts/simple-react-hw/static/js/2.902cccfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51de1dad4188e2eaf9b",
+    "url": "/React-ts/simple-react-hw/static/js/main.653045e6.chunk.js"
+  },
+  {
+    "revision": "4fe6803d91250489f711",
+    "url": "/React-ts/simple-react-hw/static/js/runtime-main.76f236a0.js"
+  },
+  {
+    "revision": "9efa5f0d9e31969dea21de3a6919f874",
+    "url": "/React-ts/simple-react-hw/static/media/404.9efa5f0d.jpg"
+  },
+  {
+    "revision": "04d12e8db7500c8d0085b57cff495e60",
+    "url": "/React-ts/simple-react-hw/static/media/ninja.04d12e8d.svg"
+  }
+]);
