ddcf422bf4559a7fd74e894ee881153ae1c94b2d
lusorich <dreamonautre@gmail.com>
2021-03-11 23:17:27 +0500

    changes

--- videos-course-1/src/redux/users-selectors.ts
+++ videos-course-1/src/redux/users-selectors.ts
@@ -2,10 +2,12 @@ export const getUsers = (state: any) => state.usersPage.users;
 
 export const getPageSize = (state: any) => state.usersPage.pageSize;
 
-export const getTotalUsersCount = (state: any) => state.usersPage.totalUsersCount;
+export const getTotalUsersCount = (state: any) =>
+  state.usersPage.totalUsersCount;
 
 export const getCurrentPage = (state: any) => state.usersPage.currentPage;
 
 export const getIsFetching = (state: any) => state.usersPage.isFetching;
 
-export const getFollowingInProgress = (state: any) => state.usersPage.followingInProgress;
+export const getFollowingInProgress = (state: any) =>
+  state.usersPage.followingInProgress;
